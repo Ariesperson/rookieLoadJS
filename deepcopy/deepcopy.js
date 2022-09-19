99200f29e96b0d07104bb5801a706581a27b2648
siyan.chen <siyan.chen@dmall.com>
2022-09-19 14:37:10 +0800

    feat:深拷贝

---
+++ deepcopy/deepcopy.js
@@ -0,0 +1,43 @@
+let obj = {
+    node:'test1',
+    data:{
+
+    },
+    props:{
+        title:{
+            type:'String'
+        }
+    },
+    methods: {
+        clickme(){
+            alert('请点击我')
+        }
+    },
+    children:[
+        {
+            node:'div',
+            props:{}
+        },
+        {
+
+        }
+    ]
+}
+const deepcopy=(source)=>{
+    let result = {}
+    for (const key in source) {
+        if(typeof source[key]=='object'){
+            result[key] = deepcopy(source[key])
+        }else if(typeof source[key]=='Array'){
+            result[key] = []
+            source[key].forEach(element => {
+                result[key].push(deepcopy(element))
+            });
+        }else{
+            result[key] = source[key]
+        }
+    }
+    return result
+}
+let copyobject = deepcopy(obj)
+console.log(copyobject)
